edc323425bdfc413a84a4a76bf5b1783762f7bd0
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-03-11 21:20:55 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <qrreader msg="QR Code generator"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import qrreader from './components/qr_reader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    qrreader
   }
 }
 </script>
